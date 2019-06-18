f5d6d1d9bb3b2e22e482a684dd73f4dab8819f6e
jobino <jobino-araujo@hotmail.com>
2019-06-17 23:40:19 -0300

    done feed instagram part web add socket io add news post add likes post

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
-
+import io from 'socket.io-client';
 import './Feed.css';
 
 import more from '../assets/more.svg';
@@ -15,17 +15,37 @@ class Feed extends Component {
     }
 
     async componentDidMount() {
+        this.registerToSocket();
         const response = await api.get('posts');
 
         this.setState({ feed: response.data });
     }
 
+    registerToSocket = () => {
+        const socket = io('http://172.17.0.3:9496');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed]});
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
 
     render() {
         return (
             <section id="post-list">
                 {this.state.feed.map(post => (
-                    <article>
+                    <article key={post._id} >
                         <header>
                             <div className="user-info">
                                 <span>{post.author}</span>
@@ -38,7 +58,9 @@ class Feed extends Component {
 
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="" />
+                                <button type="button" onClick={() => this.handleLike(post._id)} >
+                                    <img src={like} alt="" />
+                                </button>
                                 <img src={comment} alt="" />
                                 <img src={send} alt="" />
                             </div>
